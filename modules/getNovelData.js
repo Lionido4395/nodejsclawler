e8c8dd295b46f06c650f9b4db6768fadabe43a60
yingbokong <392013660@qq.com>
2016-10-13 17:34:36 +0800

    update

--- modules/getNovelData.js
+++ modules/getNovelData.js
@@ -1,68 +1,69 @@
 ;(function(){
-    var cheerio = require('cheerio')
-    var gs = require('nodegrass')
+    var cheerio = require('cheerio');
+    var gs = require('nodegrass');
     
-    var mongoose = require('mongoose')
-    mongoose.connect('mongodb://localhost/crawlerNovel')
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/crawlerNovel');
 
-    var url = 'http://www.biquku.com/0/330/'
+    var url = 'http://www.biquku.com/xiaoshuodaquan/';
 
     /**
      * 获取小说章节数据
      * @return {[json]} [小说章节列表]
      */
     var getList = function (){
-        var chapterData = []
+        var chapterData = [];
         gs.get(url,function(data){
-            var html= data
-            var $ = cheerio.load(html)
-            $('#list dd a').each(function(idx, item){
-                var $item = $(item);
+            var html= data;
+            var $ = cheerio.load(html);
+            $('#main .novellist li').each(function(idx, item){
+                var $item = $(item).find('a');
                 chapterData.push({
                     'title': $item.text(),
-                    'id': $item.attr('href').split('.')[0]
-                })
-            })
+                    'id': $item.attr('href').split('http://www.biquku.com')[1]
+                });
+                console.log(chapterData)
+            });
         },'gbk').on('error',function(err){
-            console.log(err)
-        })
-        return chapterData
+            console.log(err);
+        });
+        return chapterData;
     }
-
+    getList()
     var getContent = function (){
-        var chapterData = getList()
+        var chapterData = getList();
 
         for (var i = 0; i < chapterData.length; i++) {
-            var subUrl = url + chapterData.id + '.html'
+            var subUrl = url + chapterData.id + '.html';
             gs.get(subUrl,function(data){
-                var html= data
-                var $ = cheerio.load(html)
-                var title = chapterData.title
-                var content = $('#content').text()
+                var html= data;
+                var $ = cheerio.load(html);
+                var title = chapterData.title;
+                var content = $('#content').text();
 
                 var Novel = mongoose.model('Novel', {
                     title: String,
                     content: String
-                })
+                });
 
                 var Chapter = new Novel({
                     title: title, 
                     content: content
-                })
+                });
 
                 Chapter.save(function(err){
                     if(err){
-                        console.log('保存失败')
+                        console.log('保存失败');
                     }
-                    console.log("保存完成")
-                })
+                    console.log("保存完成");
+                });
             },'gbk').on('error',function(err){
-                console.log(err)
-            })
+                console.log(err);
+            });
         }
     }
     module.exports = {
         getList: getList,
         getContent: getContent
     }
-}).call(this)
+}).call(this);
